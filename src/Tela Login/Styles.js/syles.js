3853ac44dde1046033e312e81041e59490895554
sil-barbosa <ma.dosil82@gmail.com>
2023-09-24 17:29:13 -0300

    Add Tela de Login

---
+++ src/Tela Login/Styles.js/syles.js
@@ -0,0 +1,19 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color:'blue',
+  },
+});
+
+export default styles
