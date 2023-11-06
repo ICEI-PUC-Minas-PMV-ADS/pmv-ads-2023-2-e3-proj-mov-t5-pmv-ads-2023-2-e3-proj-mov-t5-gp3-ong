82aa65a61633b663a794785ab02480254034c7e2
Janaina Leal <73653429+JanainaLeal@users.noreply.github.com>
2023-11-06 15:40:55 +0000

    atualização de cards
    atualização de cards com imagens, descriçao e titulo

--- src/cor--vol-v3/components/Pages/Ongs.js
+++ src/cor--vol-v3/components/Pages/Ongs.js
@@ -8,58 +8,58 @@ const data = [
     site: 'www.missaoelpallet.com.br',
     telefone: '123456789',
     email: 'missao@missaoelpallet.com.br',
-    descricao: 'Descrição da ONG 1...',
-    imagem: 'https://via.placeholder.com/150',
+    descricao: 'Somos uma ONG com objetivo de semear a boa semente e cremos que todos podem ter uma segunda chance. Nosso objetivo é transformar!',
+    imagem: require('../../assets/ongs_img/ong1.jpg'),
 
   },
   {
     id: 2,
-    nome: 'ONG 2',
-    site: 'www.ong2.com',
+    nome: 'IDC 3º Milênio',
+    site: 'idcterceiromilenio.org/',
     telefone: '123456789',
-    email: 'ong1@email.com',
-    descricao: 'Descrição da ONG 2...',
-    imagem: 'https://via.placeholder.com/150', // URL da imagem da ONG
+    email: 'idc3milenio@gmail.com',
+    descricao: 'O Serviço de Acolhimento institucional para Crianças e Adolescentes (SAICA ) é um serviço de proteção social especial de alta complexidade.',
+    imagem: require('../../assets/ongs_img/ong2.png'), // URL da imagem da ONG
     
   },
   {
     id: 3,
-    nome: 'ONG 3',
+    nome: 'Dia de Doar Floripa',
     site: 'www.ong3.com',
     telefone: '123456789',
     email: 'ong1@email.com',
-    descricao: 'Descrição da ONG 3...',
-    imagem: 'https://via.placeholder.com/150', // URL da imagem da ONG
+    descricao: 'DOrganizações Sociais de Floripa, juntas, despertando a generosidade dos moradores da Ilha da Magia, celebrando o prazer que é doar. Escolha sua causa!',
+    imagem: require('../../assets/ongs_img/ong3.jpg'), // URL da imagem da ONG
     
   },
   {
     id: 4,
-    nome: 'ONG 4',
-    site: 'www.ong4.com',
+    nome: 'Espaço Cultural Nova Bonsucesso',
+    site: 'diadedoar.floripa.br/',
     telefone: '123456789',
-    email: 'ong1@email.com',
-    descricao: 'Descrição da ONG 4...',
-    imagem: 'https://via.placeholder.com/150', // URL da imagem da ONG
+    email: 'contato@compradobem.com.br',
+    descricao: 'A ONG oferece atendimento à famílias em vulnerabilidade e extrema vulnerabilidade social.',
+    imagem: require('../../assets/ongs_img/ong4.jpg'), // URL da imagem da ONG
     
   },
   {
     id: 5,
-    nome: 'ONG 5',
+    nome: 'Mansão Animalão',
     site: 'www.ong5.com',
     telefone: '123456789',
     email: 'ong1@email.com',
-    descricao: 'Descrição da ONG 5...',
-    imagem: 'https://via.placeholder.com/150', // URL da imagem da ONG
+    descricao: 'Somos um grupo de pessoas dedicadas à causa animal! Atuamos atualmente nas frentes de conscientização e vamos iniciar em breve alguns resgates.',
+    imagem: require('../../assets/ongs_img/ong5.jpg'), // URL da imagem da ONG
     
   },
   {
     id: 6,
-    nome: 'ONG 6',
+    nome: 'Semeando Amor',
     site: 'www.ong4.com',
     telefone: '123456789',
-    email: 'ong1@email.com',
-    descricao: 'Descrição da ONG 6...',
-    imagem: 'https://via.placeholder.com/150', // URL da imagem da ONG
+    email: 'institutosemeandoamor@gmail.com',
+    descricao: 'Estamos presentes na Comunidade Cesar Maia há quase 20 anos. Disponibilizamos aulas de reforço escolar e atividades culturais para jovens de 7 a 16 anos.',
+    imagem: require('../../assets/ongs_img/ong6.jpg'),// URL da imagem da ONG
     
   },
   {
@@ -105,12 +105,20 @@ const data = [
 
 const OngCard = ({ ong }) => (
   <View style={styles.card}>
-    <Image source={{ uri: ong.imagem }} style={styles.imagem} />
+    <Image source={ong.imagem} style={styles.imagem} />
     <Text style={styles.nome}>{ong.nome}</Text>
-    <Text>{ong.site}</Text>
-    <Text>{ong.telefone}</Text>
-    <Text>{ong.email}</Text>
-    <Text>{ong.descricao}</Text>
+    <Text numberOfLines={1} style={styles.text}>
+      {ong.site}
+    </Text>
+    <Text numberOfLines={1} style={styles.text}>
+      {ong.telefone}
+    </Text>
+    <Text numberOfLines={1} style={styles.text}>
+      {ong.email}
+    </Text>
+    <Text numberOfLines={3} style={styles.text}>
+      {ong.descricao}
+    </Text>
   </View>
 );
 
@@ -128,23 +136,30 @@ const Ongs = () => {
 
 const styles = StyleSheet.create({
   card: {
+    width: 165, // Ajuste a largura conforme necessário
+    height: 250, // Ajuste a altura conforme necessário
     borderWidth: 1,
     borderColor: '#ccc',
-    padding: 20,
+    padding: 10,
     margin: 10,
     borderRadius: 10,
   },
   imagem: {
     width: 100,
     height: 100,
-    borderRadius: 50,
+    borderRadius: 80,
     alignSelf: 'center',
     marginBottom: 10,
   },
   nome: {
-    fontSize: 20,
+    fontSize: 15, // Ajuste o tamanho conforme necessário
     fontWeight: 'bold',
-    marginBottom: 5,
+    marginBottom: 3,
+    textAlign: 'center',
+  },
+  text: {
+    fontSize: 11, // Ajuste o tamanho conforme necessário
+    textAlign: 'center',
   },
 });
 
