53e2a9a5a60b975166bcd78efd727ee8e109f601
sil-barbosa <ma.dosil82@gmail.com>
2023-11-19 21:01:59 -0300

    Update App.js

--- src/cor--vol-v3/App.js
+++ src/cor--vol-v3/App.js
@@ -3,34 +3,28 @@ import { NavigationContainer } from '@react-navigation/native';
 import Login from './components/Pages/Login';
 import Main from './components/Pages/Main';
 import CadastroUsuario from './components/Pages/CadastroUsuario';
+import Vagas from './components/Pages/Vagas';
 
 const Stack = createStackNavigator();
 
 function MyStack() {
   return (
+    
     <Stack.Navigator>
-      <Stack.Screen name="Login" component={Login} />
-      <Stack.Screen name="Main" component={Main} />
-      <Stack.Screen name="CadastroUsuario" component={CadastroUsuario} />
+      
+      <Stack.Screen name="Login" component={Login} options={{ headerShown: false }}/>
+      <Stack.Screen name="Main" component={Main} options={{ headerShown: false }}/>
+      <Stack.Screen name="Vagas" component={Vagas} options={{ headerShown: false }}/>
     </Stack.Navigator>
   );
 }
 
 export default function App() {
   return (
+    
     <NavigationContainer>
       <MyStack />
     </NavigationContainer>
   );
 }
-
-
-
-
-  
-  
-  
-  
-    
-
 
